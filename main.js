23ca43a6e58f3ab33e05e7bb822f183a1b33f4ad
Maame Yaa Osei <maameyaa.osei@ashesi.edu.gh>
2018-10-24 23:36:22 +0000

    Added .js file

---
+++ main.js
@@ -0,0 +1,33 @@
+// Smooth scroll for the menu
+$('.nav-menu a, #mobile-nav a').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        if (target.length) {
+            var top_space = 0;
+
+            if ($('#header').length) {
+                top_space = $('#header').outerHeight();
+
+                if( ! $('#header').hasClass('header-fixed') ) {
+                    top_space = top_space - 20;
+                }
+            }
+
+            $('html, body').animate({
+                scrollTop: target.offset().top - top_space
+            }, 1500, 'easeInOutExpo');
+
+            if ($(this).parents('.nav-menu').length) {
+                $('.nav-menu .menu-active').removeClass('menu-active');
+                $(this).closest('li').addClass('menu-active');
+            }
+
+            if ($('body').hasClass('mobile-nav-active')) {
+                $('body').removeClass('mobile-nav-active');
+                $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+                $('#mobile-body-overly').fadeOut();
+            }
+            return false;
+        }
+    }
+});
